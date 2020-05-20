02157f0be584299bef8e8054f59bab3f9edec2f7
Pablo Tabares H <pablother3@gmail.com>
2020-05-19 21:47:22 -0500

    first exercises

---
+++ app.py
@@ -0,0 +1,12 @@
+#name = input('what is your name? ')
+#favorite_color = input('What is your favorite color? ')
+#print('%s likes %s' % (name, favorite_color))
+
+birth_year = input('Birth ')
+age = 2020 - int(birth_year)
+
+print(age)
+
+weight = input('Weight in lbs ')
+weight_kg = int(weight) * 0.45
+print(weight_kg)
