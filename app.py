fd8368cc27b593547d27d6e5b177ef6194affe32
Pablo Tabares H <pablother3@gmail.com>
2020-05-20 22:23:22 -0500

    maths

--- app.py
+++ app.py
@@ -2,11 +2,40 @@
 #favorite_color = input('What is your favorite color? ')
 #print('%s likes %s' % (name, favorite_color))
 
-birth_year = input('Birth ')
-age = 2020 - int(birth_year)
+#birth_year = input('Birth ')
+#age = 2020 - int(birth_year)
 
-print(age)
+#print(age)
 
-weight = input('Weight in lbs ')
-weight_kg = int(weight) * 0.45
-print(weight_kg)
+#weight = input('Weight in lbs ')
+#weight_kg = int(weight) * 0.45
+#print(weight_kg)
+
+multiline = '''
+    Here 
+    a 
+    big text
+'''
+
+#print(multiline)
+
+stringy = 'pablo'
+#print(stringy[-2]) #l
+
+#concate with the prefix  -> f''
+first = 'goku'
+second = 'gohan'
+msg = f'{first} and {second} are familiars'
+
+#print(msg)
+
+
+
+#maths
+print(round(2.9))
+print(abs(-2.9))
+
+import math
+
+print(math.ceil(2.3))
+print(math.floor(2.8))
