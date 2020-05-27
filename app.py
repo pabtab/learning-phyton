9f823b7a83593096cd10e24982db6f0fe0aad8a2
Pablo Tabares H <pablother3@gmail.com>
2020-05-26 20:25:19 -0500

    pathlib

--- app.py
+++ app.py
@@ -1,1 +1,16 @@
+import random
 
+for i in range(3):
+    print(random.randint(10, 20))
+
+members = ['John', 'pablo', 'goku']
+
+print(random.choice(members))
+
+
+from pathlib import Path
+
+path = Path()
+
+for file in path.glob('*.py'):
+    print(file)
